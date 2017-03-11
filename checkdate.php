b85f4e71fc2712299827f47e80dd94d1c296a859
Kacper Pieszyk <kacpie@st.amu.edu.pl>
2017-03-11 11:48:01 +0100

    Formatting

--- checkdate.php
+++ checkdate.php
@@ -3,12 +3,11 @@ $jsonFile = url_get_contents('http://wmitimetable.herokuapp.com/schedules.json')
 $json = json_decode($jsonFile, true);
 $schedulesFile = file_get_contents('schedules.json');
 $schedules = json_decode($schedulesFile, true);
-
 $result = array_recursive_diff($json, $schedules);
 $date = new DateTime();
 $date = $date->format("Y-m-d G:i:s");
 
-if(empty($result)) {
+if (empty($result)) {
 	echo 'No changes';
 	if ($lastChecked = fopen('lastChecked.date', 'w')) {
 		fwrite($lastChecked, $date);
@@ -22,11 +21,12 @@ if(empty($result)) {
 		fwrite($newJsonFile, $jsonFile);
 		fwrite($newJsonFile, PHP_EOL);
 	}
-	echo 'Updating date '.$date;
+	echo 'Updating date ' . $date;
 }
 
-function url_get_contents ($url) {
-	if (!function_exists('curl_init')){
+function url_get_contents($url)
+{
+	if (!function_exists('curl_init')) {
 		die('CURL is not installed!');
 	}
 	$ch = curl_init();
@@ -37,23 +37,26 @@ function url_get_contents ($url) {
 	return $output;
 }
 
-function array_recursive_diff($array1, $array2) {
-    $aReturn = array();
-
-    foreach ($array1 as $mKey => $mValue) {
-        if (array_key_exists($mKey, $array2)) {
-            if (is_array($mValue)) {
-                $aRecursiveDiff = array_recursive_diff($mValue, $array2[$mKey]);
-                if (count($aRecursiveDiff)) { $aReturn[$mKey] = $aRecursiveDiff; }
-            } else {
-                if ($mValue != $array2[$mKey]) {
-                    $aReturn[$mKey] = $mValue;
-                }
-            }
-        } else {
-            $aReturn[$mKey] = $mValue;
-        }
-    }
-    return $aReturn;
+function array_recursive_diff($array1, $array2)
+{
+	$aReturn = array();
+	
+	foreach ($array1 as $mKey => $mValue) {
+		if (array_key_exists($mKey, $array2)) {
+			if (is_array($mValue)) {
+				$aRecursiveDiff = array_recursive_diff($mValue, $array2[$mKey]);
+				if (count($aRecursiveDiff)) {
+					$aReturn[$mKey] = $aRecursiveDiff;
+				}
+			} else {
+				if ($mValue != $array2[$mKey]) {
+					$aReturn[$mKey] = $mValue;
+				}
+			}
+		} else {
+			$aReturn[$mKey] = $mValue;
+		}
+	}
+	return $aReturn;
 }
 ?>
